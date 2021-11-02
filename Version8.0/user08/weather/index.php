4ab2c099f386a539612f230f3693f5a83d1e6ed9
DillanBorchardt <310325@shakopeeschools.org>
2021-11-02 10:21:08 -0500

    Made changes to website to improve many things like content
    made changes to the layout and other things

--- Version8.0/user08/weather/index.php
+++ Version8.0/user08/weather/index.php
@@ -1,5 +1,5 @@
 <?php
-$apiKey = "API KEY"; //You will need to add in the 
+$apiKey = "78eecbb8ff9013f0946ac16ee3eabe68"; //You will need to add in the 
 $cityId = "5046997"; //5046997 Shakopee City Id
 $units = "metric";//metric-Celcius  imperial-Farhenheit
 if ($units == 'metric'){//Changes the $temp varaible to match 
