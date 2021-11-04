cd080a0644863f63c8d976187e34ed8de2e63574
DillanBorchardt <310325@shakopeeschools.org>
2021-11-04 10:27:15 -0500

    rearanged where files are kept so api works
    api did't fully work and had to be rearanged in files

--- Version8.0/user08/weather/index.php
+++ Version8.0/user08/weather/index.php
@@ -1,7 +1,7 @@
 <?php
 $apiKey = "78eecbb8ff9013f0946ac16ee3eabe68"; //You will need to add in the 
 $cityId = "5046997"; //5046997 Shakopee City Id
-$units = "metric";//metric-Celcius  imperial-Farhenheit
+$units = "imperial";//metric-Celcius  imperial-Farhenheit
 if ($units == 'metric'){//Changes the $temp varaible to match 
     $temp = "C";
 }
